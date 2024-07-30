8183c37100418b53fa59cb55c68a42f82f12eaca
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2024-07-30 14:29:15 +0200

    mxim

--- src/main/java/com/api/sportyShoes/SportyShoesApplication.java
+++ src/main/java/com/api/sportyShoes/SportyShoesApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportyShoesApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(SportyShoesApplication.class, args);
-		System.out.println("Apllication Started Successfully");
+		System.out.println("Application Started Successfully");
 	}
 
 }
